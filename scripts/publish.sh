a85f84667364a8a7ed329829517bb120f1774f4f
season <997432833lixuan@gmail.com>
2023-07-26 00:39:59 +0800

    chore: update publish sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 pnpm lint:fix
+git add .
+git commit -m "style: eslint lint"
+git push --all
 
 find $PWD/packages -type d  -maxdepth 1 | while read folder_path; do
   echo "Publish packages path for $folder_path"
