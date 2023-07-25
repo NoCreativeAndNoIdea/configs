55679850d4bf3dfca7dd307ae209f05938f02024
season <997432833lixuan@gmail.com>
2023-07-26 00:49:08 +0800

    chore: publish shell

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,11 +5,13 @@ git add .
 git commit -m "style: eslint lint"
 git push --all
 
-find $PWD/packages/ -type d  -maxdepth 1 | while read folder_path; do
-  echo "==========Publish Start=========="
-  echo "Publish packages path for $folder_path"
-  cd $folder_path && pnpm publish --tag latest --dry-run --access public --otp=$1
-  echo "==========Publish End=========="
+find $PWD/packages -type d  -maxdepth 1 | while read folder_path; do
+  if [ -f "$folder_path/package.json" ]; then
+    echo "==========Publish Start=========="
+    echo "Publish packages path for $folder_path"
+    cd $folder_path && pnpm publish --tag latest --dry-run --access public --otp=$1
+    echo "==========Publish End=========="
+  fi
 done
 
 git push --all
