1eb57a121219640385f6af1388e0f4c9fedbfc3f
season <997432833lixuan@gmail.com>
2023-07-26 00:47:08 +0800

    chore: publish success

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,9 +9,9 @@ find $PWD/packages -type d  -maxdepth 1 | while read folder_path; do
   if [ -f "$folder_path/package.json" ]; then
     echo "==========Publish Start=========="
     echo "Publish packages path for $folder_path"
-    cd $folder_path && pnpm publish --tag latest --dry-run --access public --otp=$1
+    cd $folder_path && pnpm publish --tag latest --access public --otp=$1
     echo "==========Publish End=========="
   fi
 done
 
-git push --all
+git push --all --force
