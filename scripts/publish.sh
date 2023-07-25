f32a37ef61e8bc33c05432c852575a16bd306e82
season <997432833lixuan@gmail.com>
2023-07-26 00:45:16 +0800

    chore: update publish message

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,8 +6,10 @@ git commit -m "style: eslint lint"
 git push --all
 
 find $PWD/packages -type d  -maxdepth 1 | while read folder_path; do
+  echo "\n"
   echo "Publish packages path for $folder_path"
   cd $folder_path && pnpm publish --tag latest --dry-run --access public --otp=$1
+  echo "\n"
 done
 
 git push --all
