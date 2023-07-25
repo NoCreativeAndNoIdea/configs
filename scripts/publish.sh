a6685277da35297f45155335e9e2ddcb62b6291c
season <997432833lixuan@gmail.com>
2023-07-26 00:47:56 +0800

    chore: publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,7 +5,7 @@ git add .
 git commit -m "style: eslint lint"
 git push --all
 
-find $PWD/packages -type d  -maxdepth 1 | while read folder_path; do
+find $PWD/packages/ -type d  -maxdepth 1 | while read folder_path; do
   echo "==========Publish Start=========="
   echo "Publish packages path for $folder_path"
   cd $folder_path && pnpm publish --tag latest --dry-run --access public --otp=$1
